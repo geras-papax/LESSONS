aa209331b7743b311669bfad74cf5202c51261e0
Gerasimos Papachronopoulos <58421904+geras-papax@users.noreply.github.com>
2023-05-23 21:15:53 +0300

    Update repo struction

---
+++ Utils/CorrelationFiltering.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import ttest_ind
+
+
+def calcDrop(res:pd.DataFrame=None) -> list:
+    '''
+        Calculate which variables to drop
+    '''
+    # All variables with correlation > cutoff
+    all_corr_vars = list(set(res['v1'].tolist() + res['v2'].tolist()))
+    
+    # All unique variables in drop column
+    poss_drop = list(set(res['drop'].tolist()))
+
+    # Keep any variable not in drop column
+    keep = list(set(all_corr_vars).difference(set(poss_drop)))
+     
+    # Drop any variables in same row as a keep variable
+    p = res[ res['v1'].isin(keep)  | res['v2'].isin(keep) ][['v1', 'v2']]
+    q = list(set(p['v1'].tolist() + p['v2'].tolist()))
+    drop = (list(set(q).difference(set(keep))))
+
+    # Remove drop variables from possible drop 
+    poss_drop = list(set(poss_drop).difference(set(drop)))
+    
+    # subset res dataframe to include possible drop pairs
+    m = res[ res['v1'].isin(poss_drop)  | res['v2'].isin(poss_drop) ][['v1', 'v2','drop']]
+        
+    # remove rows that are decided (drop), take set and add to drops
+    more_drop = set(list(m[~m['v1'].isin(drop) & ~m['v2'].isin(drop)]['drop']))
+    for item in more_drop:
+        drop.append(item)
+         
+    return drop
+
+
+
+def corrX(df:pd.DataFrame=None, cut:int=0.9) -> list:
+    '''
+        Log the variable states based on the original logic
+    '''
+
+    # Get correlation matrix and upper triagle
+    corr_mtx = df.corr().abs()
+    avg_corr = corr_mtx.mean(axis = 1)
+    up = corr_mtx.where(np.triu(np.ones(corr_mtx.shape), k=1).astype(np.bool))
+    
+    dropcols = list()
+    
+    res = pd.DataFrame(columns=(['v1', 'v2', 'v1.target', 'v2.target','corr', 'drop' ]))
+    
+    for row in range(len(up)-1):
+        col_idx = row + 1
+        for col in range (col_idx, len(up)):
+            if(corr_mtx.iloc[row, col] > cut):
+                if(avg_corr.iloc[row] > avg_corr.iloc[col]): 
+                    dropcols.append(row)
+                    drop = corr_mtx.columns[row]
+                else: 
+                    dropcols.append(col)
+                    drop = corr_mtx.columns[col]
+                
+                s = pd.Series([ corr_mtx.index[row],
+                up.columns[col],
+                avg_corr[row],
+                avg_corr[col],
+                up.iloc[row,col],
+                drop],
+                index = res.columns)
+        
+                res = res.append(s, ignore_index = True)
+    
+    dropcols_names = calcDrop(res)
+    
+    return(dropcols_names)
+
+
+'''
+Example - How to use
+
+df = pd.read_csv( 'Data/boston_corrected.txt' )
+
+corrX_new(df, cut = 0.7)
+
+'''
+
+def PearsonCorrelation(data:pd.DataFrame=None) -> pd.DataFrame:
+  '''
+  Calculate correlation between feature columns
+  '''
+  correlation_matrix = data.iloc[:, :-1].corr()
+
+  # Set the range between 0 and 1
+  correlation_matrix = (correlation_matrix + 1)/2
+  # Visualize the correlations
+  plt.figure(figsize=(19, 15))
+  sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
+  plt.title("Pearson's Correlation Heatmap")
+  plt.show()
+
+  return correlation_matrix
+
+
+def CorrelatingFiltering(data: pd.DataFrame=None, correlation_matrix: pd.DataFrame=None) -> tuple[pd.DataFrame, bool]:
+  # Avg correlation for each feature
+  average_correlations = correlation_matrix.abs().mean()
+
+  # Highest avg correlation
+  feature_with_max_average_correlation = average_correlations.idxmax()
+  max_average_correlation_value = average_correlations.max()
+
+  print("Feature with the highest average correlation:", feature_with_max_average_correlation)
+  print("Max average correlation value:", max_average_correlation_value)
+
+  # Get the columns with correlation lower than 0.8 with the specific column
+  low_correlation_columns = correlation_matrix[(correlation_matrix[feature_with_max_average_correlation] < 0.8) | (correlation_matrix[feature_with_max_average_correlation] == 1)].index
+
+  # Last column with labels
+  low_correlation_columns = low_correlation_columns.append(pd.Index([data.columns[-1]]))
+  
+  # Filter the columns and keep the desired ones
+  data_filtered = data[low_correlation_columns]
+
+  # Confirm the dropped columns
+  dropped_columns = data.columns.difference(data_filtered.columns)
+  if dropped_columns.empty:
+    return data, False
+  else:
+    print("Dropped columns:", dropped_columns)
+    return data_filtered, True
+
---
+++ Utils/DataAnalysis.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import ttest_ind
+
+
+def FeatureInfo(data: pd.DataFrame=None):
+  categorical_cols, non_categorical_cols = checkCategoricalColumns(data)
+  # Get statistics of the features
+  statistics = data.describe()
+  print(statistics)
+
+  # Number of rows and columns for the grid
+  num_features = len(data.columns) - 1  
+  num_rows = (num_features - 1) // 3 + 1
+  num_cols = min(3, num_features)
+
+  fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, 8))
+  axes = axes.flatten()
+
+  # Distribution of each feature
+  for i, column in enumerate(data.columns):
+      if column not in categorical_cols:  
+          sns.histplot(data[column], kde=True, ax=axes[i])
+          axes[i].set_xlabel(column)
+          axes[i].set_ylabel('Frequency')
+          axes[i].set_title(f'Distribution of {column}')
+
+  fig.tight_layout()
+  plt.show()
+
+  # Label's class distribution
+
+  plt.figure(figsize=(8, 6))
+  sns.countplot(data=data, x='label')
+  plt.xlabel('Label')
+  plt.ylabel('Count')
+  plt.title('Class Distribution')
+  plt.show()
+
+  # Plot pair-wise interactions between features with label
+  sns.pairplot(data      = data, 
+             hue       = 'label',
+             palette   = 'Set1',
+             kind      = 'scatter', # 'scatter', 'kde', 'hist', 'reg'
+             diag_kind = 'hist');   # 'auto', 'hist', 'kde', None
+  plt.show()
+
+
+def T_Test(data: pd.DataFrame=None, categorical_cols: list=None, non_categorical_cols: list=None):
+  feature_columns = data.drop(categorical_cols[0], axis=1)
+  categorical_column = data[categorical_cols[0]]
+  '''
+  Perform T-Test to compare the means of the numerical feature column 
+  for the two categories of the binary categorical column
+  '''
+  ttest_results = []
+  for column in feature_columns.columns:
+      category1 = feature_columns[categorical_column == 0][column]
+      category2 = feature_columns[categorical_column == 1][column]
+      t_stat, p_value = ttest_ind(category1, category2)
+      print(t_stat, p_value)
+      ttest_results.append({'Feature': column, 'T-Statistic': t_stat, 'P-Value': p_value})
+
+  ttest_results = pd.DataFrame(ttest_results)
+
+  plt.figure(figsize=(10, 6))
+  sns.barplot(x='Feature', y='T-Statistic', data=ttest_results, color='blue', alpha=0.5, label='T-Statistic')
+  plt.xticks(rotation=90)
+  plt.xlabel('Feature')
+  plt.ylabel('Value')
+  plt.title('T-Test Results: Feature Columns vs. Categorical Column')
+  plt.legend()
+  plt.show()
+
+
+def checkOutliers(data: pd.DataFrame=None, numerical_columns:list=None):
+  '''
+  Checking for outliers in each feature
+  '''
+  plt.figure(figsize=(8, 3 * len(numerical_columns)))
+  for i, column in enumerate(numerical_columns):
+      plt.subplot(len(numerical_columns), 2, 2*i+1)
+      sns.boxplot(x=data[column])
+      plt.xlabel(column)
+      plt.title('Boxplot - {}'.format(column))
+
+      plt.subplot(len(numerical_columns), 2, 2*i+2)
+      sns.histplot(data[column], kde=False)
+      plt.xlabel(column)
+      plt.ylabel('Frequency')
+      plt.title('Distribution Plot - {}'.format(column))
+
+  plt.tight_layout()
+  plt.show()
+
+
+def isCategorical(column: pd.DataFrame=None) -> bool:
+    unique_values = column.unique()
+    num_unique_values = len(unique_values)
+    num_total_values = len(column)
+    '''
+    If the number of unique values is relatively small 
+    compared to the total values, it's likely categorical
+    '''
+    threshold = 0.05  # threshold based on dataset
+
+    if num_unique_values / num_total_values <= threshold:
+        return True
+    else:
+        return False
+
+def checkCategoricalColumns(data: pd.DataFrame=None) -> tuple[list, list]:
+    categorical_columns = []
+    non_categorical_columns = []
+
+    for column in data.columns:
+        is_cat = isCategorical(data[column])
+        if is_cat:
+            categorical_columns.append(column)
+        else:
+            non_categorical_columns.append(column)
+
+    return categorical_columns, non_categorical_columns
