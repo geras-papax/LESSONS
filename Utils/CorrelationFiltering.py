49438385da1f246edd4f7adf0c36a6c31743b2f5
Gerasimos Papachronopoulos <58421904+geras-papax@users.noreply.github.com>
2023-05-28 21:26:03 +0300

    ADD doc-string, update notebook eda

--- Utils/CorrelationFiltering.py
+++ Utils/CorrelationFiltering.py
@@ -8,7 +8,18 @@ from ennemi import pairwise_mi
 
 def calcDrop(res:pd.DataFrame=None) -> list:
     '''
-        Calculate which variables to drop
+    Calculate which variables to drop based on the correlation analysis.
+
+    Parameters
+    ----------
+    res : pandas.DataFrame, optional
+        DataFrame containing the correlation analysis results (default: None).
+
+    Returns
+    -------
+    list
+        A list of column names to be dropped based on the correlation analysis.
+
     '''
     # All variables with correlation > cutoff
     all_corr_vars = list(set(res['v1'].tolist() + res['v2'].tolist()))
@@ -41,7 +52,20 @@ def calcDrop(res:pd.DataFrame=None) -> list:
 
 def corrX(df:pd.DataFrame=None, cut:int=0.9) -> list:
     '''
-        Log the variable states based on the original logic
+    Identify highly correlated variables and recommend columns to drop.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame, optional
+        Input DataFrame containing the data (default: None).
+    cut : int, optional
+        Threshold for correlation value (default: 0.9).
+
+    Returns
+    -------
+    list
+        A list of column names to be dropped based on high correlation.
+
     '''
 
     # Get correlation matrix and upper triagle
@@ -89,21 +113,45 @@ corrX_new(df, cut = 0.7)
 '''
 
 def PearsonCorrelation(data:pd.DataFrame=None):
-  '''
-  Calculate correlation between feature columns
-  '''
-  correlation_matrix = abs(data.iloc[:, :-1].corr())
+    '''
+    Calculate and visualize the Pearson's correlation matrix.
 
-  # Set the range between 0 and 1
-  # correlation_matrix = (correlation_matrix + 1)/2
-  # Visualize the correlations
-  plt.figure(figsize=(19, 15))
-  sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
-  plt.title("Pearson's Correlation Heatmap")
-  plt.show()
+    Parameters
+    ----------
+    data : pandas.DataFrame, optional
+        Input DataFrame containing the data (default: None).
+
+    Returns
+    -------
+    None
+        This function does not return any value. It displays the Pearson's correlation heatmap.
+
+    '''
+    correlation_matrix = abs(data.iloc[:, :-1].corr())
+
+    # Set the range between 0 and 1
+    # correlation_matrix = (correlation_matrix + 1)/2
+    # Visualize the correlations
+    plt.figure(figsize=(19, 15))
+    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
+    plt.title("Pearson's Correlation Heatmap")
+    plt.show()
 
 def FiCorrelation(data:pd.DataFrame=None):
+    '''
+    Calculate and visualize the Phi_k correlation matrix.
 
+    Parameters
+    ----------
+    data : pandas.DataFrame, optional
+        Input DataFrame containing the data (default: None).
+
+    Returns
+    -------
+    None
+        This function does not return any value. It displays the Phi_k correlation heatmap.
+
+    '''
     # Calculate phi_k correlation matrix
     correlation_matrix = data.iloc[:, :-1].phik_matrix()
 
@@ -114,18 +162,29 @@ def FiCorrelation(data:pd.DataFrame=None):
 
 
 def MutualInformation(data:pd.DataFrame=None):
+    '''
+    Calculate and visualize mutual information between variables.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame, optional
+        Input DataFrame containing the data (default: None).
+
+    Returns
+    -------
+    None
+        This function does not return any value. It displays the mutual information results.
+
+    '''
     # Compare target variable against all other variables
-    #
     estimate = estimate_mi(y= data['label'].values, 
                         x= data.iloc[:,:-1],
                         normalize = True)
     # Pairwise comparisons between a set of variables
-    #
     pairwise = pairwise_mi( data, normalize=True )
     plt.figure(figsize=(12, 7))
     sns.barplot(data=estimate, orient='h')
     # Visualize Mutual Information matrix
-    #
     plt.figure(figsize = (22, 15))
 
     sns.heatmap(pairwise, annot=True, cmap="coolwarm")
@@ -133,36 +192,23 @@ def MutualInformation(data:pd.DataFrame=None):
     plt.title('Mutual Information matrix showing correlation coefficients')
     plt.show()
 
-
-def CorrelatingFiltering(data: pd.DataFrame=None, correlation_matrix: pd.DataFrame=None) -> '''tuple[pd.DataFrame, bool]''':
-  # Avg correlation for each feature
-  average_correlations = correlation_matrix.abs().mean()
-
-  # Highest avg correlation
-  feature_with_max_average_correlation = average_correlations.idxmax()
-  max_average_correlation_value = average_correlations.max()
-
-  print("Feature with the highest average correlation:", feature_with_max_average_correlation)
-  print("Max average correlation value:", max_average_correlation_value)
-
-  # Get the columns with correlation lower than 0.8 with the specific column
-  low_correlation_columns = correlation_matrix[(correlation_matrix[feature_with_max_average_correlation] < 0.8) |  (correlation_matrix[feature_with_max_average_correlation] == 1)].index
-
-  # Last column with labels
-  low_correlation_columns = low_correlation_columns.append(pd.Index([data.columns[-1]]))
-  
-  # Filter the columns and keep the desired ones
-  data_filtered = data[low_correlation_columns]
-
-  # Confirm the dropped columns
-  dropped_columns = data.columns.difference(data_filtered.columns)
-  if dropped_columns.empty:
-    return data, False
-  else:
-    print("Dropped columns:", dropped_columns)
-    return data_filtered, True
-
 def FeatureSelection(data: pd.DataFrame=None, num: int=5) :
+    '''
+    Perform feature selection using mutual information and phi_k correlation methods.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame, optional
+        Input DataFrame containing the data (default: None).
+    num : int, optional
+        Number of top features to select (default: 5).
+
+    Returns
+    -------
+    None
+        This function does not return any value. It displays the top features selected by each method.
+
+    '''
     # Separate the features and target variable
     X = data.iloc[:, :-1]  # Features
     y = data['label']  # Target variable
@@ -180,19 +226,9 @@ def FeatureSelection(data: pd.DataFrame=None, num: int=5) :
     top_correlation_indices = phik_correlation_scores.argsort()[-k:][::-1]
     top_correlation_features = X.columns[top_correlation_indices]
 
-    # Perform feature selection using Pearson correlation
-    pearson_correlation_scores = abs(data.corr(method='pearson')['label']).drop('label')
-    top_pearson_correlation_indices = pearson_correlation_scores.argsort()[-k:][::-1]
-    top_pearson_correlation_features = X.columns[top_pearson_correlation_indices]
-
     # Print the top features selected by each method
     print("Top", k, "features selected by Mutual Information:", top_mutual_info_features)
     print("Top", k, "features selected by Phi_k Correlation:", top_correlation_features)
-    print("Top", k, "features selected by Pearson Correlation:", top_pearson_correlation_features)
-    plt.figure(figsize=(12, 7))
-    sns.barplot(data=pd.DataFrame(pearson_correlation_scores).T, orient='h')
-    plt.title('Pearson Correlation on Label')
-    plt.show()
     plt.figure(figsize=(12, 7))
     sns.barplot(data=pd.DataFrame(phik_correlation_scores).T, orient='h')
     plt.title('Phi_k Correlation on Label')
--- Utils/DataAnalysis.py
+++ Utils/DataAnalysis.py
@@ -6,104 +6,117 @@ from scipy.stats import ttest_ind
 
 
 def FeatureInfo(data: pd.DataFrame=None):
-  categorical_cols, non_categorical_cols = checkCategoricalColumns(data)
-  # Get statistics of the features
-  statistics = data.describe()
-  print(statistics)
+  
+    '''
+    Display feature information and visualizations.
 
-  # Number of rows and columns for the grid
-  num_features = len(data.columns) - 1  
-  num_rows = (num_features - 1) // 3 + 1
-  num_cols = min(3, num_features)
+    Parameters
+    ----------
+    data : pandas.DataFrame, optional
+        Input DataFrame containing the data (default: None).
 
-  fig, axes = plt.subplots(num_rows, num_cols, figsize=(18, 16))
-  axes = axes.flatten()
+    Returns
+    -------
+    None
+        This function does not return any value. It displays visualizations.
 
-  # Distribution of each feature
-  for i, column in enumerate(data.columns):
-      if column not in categorical_cols:  
-          sns.histplot(data[column], kde=True, ax=axes[i])
-          axes[i].set_xlabel(column)
-          axes[i].set_ylabel('Frequency')
-          axes[i].set_title(f'Distribution of {column}')
+    '''
 
-  plt.tight_layout()
-  plt.show()
+    categorical_cols, non_categorical_cols = checkCategoricalColumns(data)
+    # Get statistics of the features
+    statistics = data.describe()
+    print(statistics)
 
-  # Label's class distribution
+    # Number of rows and columns for the grid
+    num_features = len(data.columns) - 1  
+    num_rows = (num_features - 1) // 3 + 1
+    num_cols = min(3, num_features)
 
-  plt.figure(figsize=(8, 6))
-  sns.countplot(data=data, x='label')
-  plt.xlabel('Label')
-  plt.ylabel('Count')
-  plt.title('Class Distribution')
-  plt.show()
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(18, 16))
+    axes = axes.flatten()
 
-#   # Plot pair-wise interactions between features with label
-#   sns.pairplot(data      = data, 
-#              hue       = 'label',
-#              palette   = 'Set1',
-#              kind      = 'scatter', # 'scatter', 'kde', 'hist', 'reg'
-#              diag_kind = 'hist');   # 'auto', 'hist', 'kde', None
-#   plt.show()
+    # Distribution of each feature
+    for i, column in enumerate(data.columns):
+        if column not in categorical_cols:  
+            sns.histplot(data[column], kde=True, ax=axes[i])
+            axes[i].set_xlabel(column)
+            axes[i].set_ylabel('Frequency')
+            axes[i].set_title(f'Distribution of {column}')
 
+    plt.tight_layout()
+    plt.show()
 
-def T_Test(data: pd.DataFrame=None, categorical_cols: list=None, non_categorical_cols: list=None):
-  feature_columns = data.drop(categorical_cols[0], axis=1)
-  categorical_column = data[categorical_cols[0]]
-  '''
-  Perform T-Test to compare the means of the numerical feature column 
-  for the two categories of the binary categorical column
-  '''
-  ttest_results = []
-  for column in feature_columns.columns:
-      category1 = feature_columns[categorical_column == 0][column]
-      category2 = feature_columns[categorical_column == 1][column]
-      t_stat, p_value = ttest_ind(category1, category2)
-      print(t_stat, p_value)
-      ttest_results.append({'Feature': column, 'T-Statistic': t_stat, 'P-Value': p_value})
+    # Label's class distribution
 
-  ttest_results = pd.DataFrame(ttest_results)
+    plt.figure(figsize=(8, 6))
+    sns.countplot(data=data, x='label')
+    plt.xlabel('Label')
+    plt.ylabel('Count')
+    plt.title('Class Distribution')
+    plt.show()
 
-  plt.figure(figsize=(10, 6))
-  sns.barplot(x='Feature', y='T-Statistic', data=ttest_results, color='blue', alpha=0.5, label='T-Statistic')
-  plt.xticks(rotation=90)
-  plt.xlabel('Feature')
-  plt.ylabel('Value')
-  plt.title('T-Test Results: Feature Columns vs. Categorical Column')
-  plt.legend()
-  plt.show()
+    #   # Plot pair-wise interactions between features with label
+    #   sns.pairplot(data      = data, 
+    #              hue       = 'label',
+    #              palette   = 'Set1',
+    #              kind      = 'scatter', # 'scatter', 'kde', 'hist', 'reg'
+    #              diag_kind = 'hist');   # 'auto', 'hist', 'kde', None
+    #   plt.show()
 
 
 def checkOutliers(data: pd.DataFrame=None, numerical_columns:list=None):
-  '''
-  Checking for outliers in each feature
-  '''
-  plt.figure(figsize=(8, 3 * len(numerical_columns)))
-  for i, column in enumerate(numerical_columns):
-      plt.subplot(len(numerical_columns), 2, 2*i+1)
-      sns.boxplot(x=data[column])
-      plt.xlabel(column)
-      plt.title('Boxplot - {}'.format(column))
+    '''
+    Check for outliers in each numerical feature and visualize the results.
 
-      plt.subplot(len(numerical_columns), 2, 2*i+2)
-      sns.histplot(data[column], kde=False)
-      plt.xlabel(column)
-      plt.ylabel('Frequency')
-      plt.title('Distribution Plot - {}'.format(column))
+    Parameters
+    ----------
+    data : pandas.DataFrame, optional
+        Input DataFrame containing the data (default: None).
+    numerical_columns : list, optional
+        List of numerical columns to check for outliers (default: None).
 
-  plt.tight_layout()
-  plt.show()
+    Returns
+    -------
+    None
+        This function does not return any value. It displays visualizations.
+
+    '''
+    plt.figure(figsize=(8, 3 * len(numerical_columns)))
+    for i, column in enumerate(numerical_columns):
+        plt.subplot(len(numerical_columns), 2, 2*i+1)
+        sns.boxplot(x=data[column])
+        plt.xlabel(column)
+        plt.title('Boxplot - {}'.format(column))
+
+        plt.subplot(len(numerical_columns), 2, 2*i+2)
+        sns.histplot(data[column], kde=False)
+        plt.xlabel(column)
+        plt.ylabel('Frequency')
+        plt.title('Distribution Plot - {}'.format(column))
+
+    plt.tight_layout()
+    plt.show()
 
 
 def isCategorical(column: pd.DataFrame=None) -> bool:
+    '''
+    Check if a column is categorical based on the number of unique values.
+
+    Parameters
+    ----------
+    column : pandas.DataFrame, optional
+        Input column for which to check if it is categorical (default: None).
+
+    Returns
+    -------
+    bool
+        True if the column is determined to be categorical, False otherwise.
+
+    '''
     unique_values = column.unique()
     num_unique_values = len(unique_values)
     num_total_values = len(column)
-    '''
-    If the number of unique values is relatively small 
-    compared to the total values, it's likely categorical
-    '''
+    
     threshold = 0.05  # threshold based on dataset
 
     if num_unique_values / num_total_values <= threshold:
@@ -112,6 +125,22 @@ def isCategorical(column: pd.DataFrame=None) -> bool:
         return False
 
 def checkCategoricalColumns(data: pd.DataFrame=None) -> '''tuple[list, list]''':
+    '''
+    Check categorical and non-categorical columns in a DataFrame.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame, optional
+        Input DataFrame to check for categorical columns (default: None).
+
+    Returns
+    -------
+    tuple
+        A tuple containing two lists: 
+        - The first list contains the names of the categorical columns.
+        - The second list contains the names of the non-categorical columns.
+
+    '''
     categorical_columns = []
     non_categorical_columns = []
 
